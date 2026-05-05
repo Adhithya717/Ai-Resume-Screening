06bdadc24da320a38fc7e76172ac523a7192cf3a
Adhithya717 <adhithya.mcc.aiml.tinos@gmail.com>
2026-05-05 14:46:35 +0530

    added frontend URL to CORS

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import axios from 'axios'
 import './App.css'
 
-const API_BASE = 'http://127.0.0.1:8000/api'
+const API_BASE = 'https://airesumescreening.onrender.com'
 
 function App() {
   const [jobs, setJobs] = useState([])
